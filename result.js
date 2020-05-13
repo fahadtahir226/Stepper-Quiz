e60236306fccf002bc69e32631bbe46828426524
Max <muhammadfahad325302@gmail.com>
2020-05-13 21:28:44 +0500

    Languague Finalized

--- result.js
+++ result.js
@@ -16,23 +16,43 @@
           var Data = res.data();
 
           var table = document.getElementById('tableBody');
-          table.innerHTML = ` <tr>
-                                <td>Visual (V)</td>
-                                <td><span style="background: #f79647; color: white; padding: 10px; padding-right: ${(Data.V_value * 10)}px">${Data.V_value}</span></td>
-                              </tr>
-                              <tr>
-                                <td>Auditory (A)</td>
-                                <td><span style="background: #4f81bc; color: white; padding: 10px; padding-right: ${(Data.A_value * 10)}px">${Data.A_value}</span></td>
-                              </tr>
-                              <tr>
-                                <td>Kinesthetic (K)</td>
-                                <td><span style="background: #c0504e; color: white; padding: 10px; padding-right: ${(Data.K_value * 10)}px">${Data.K_value}</span></td>
-                              </tr>
-                              <tr>
-                                <td>Auditory-digital (Ad)</td>
-                                <td><span style="background: #9bbb58; color: white; padding: 10px; padding-right: ${(Data.Ad_value * 10)}px">${Data.Ad_value}</span></td>
-                              </tr>`;
-                              drawBasic(Data);
+          if(document.getElementById('bodyTag').classList.contains('vit')){
+            table.innerHTML = ` <tr>
+            <td>Trực quan (V)</td>
+            <td><span style="background: #f79647; color: white; padding: 10px; padding-right: ${(Data.V_value * 10)}px">${Data.V_value}</span></td>
+            </tr>
+            <tr>
+            <td>Thính giác (A)</td>
+            <td><span style="background: #4f81bc; color: white; padding: 10px; padding-right: ${(Data.A_value * 10)}px">${Data.A_value}</span></td>
+            </tr>
+            <tr>
+            <td>Động học (K)</td>
+            <td><span style="background: #c0504e; color: white; padding: 10px; padding-right: ${(Data.K_value * 10)}px">${Data.K_value}</span></td>
+            </tr>
+            <tr>
+            <td>Thính giác-kỹ thuật số (Ad)</td>
+            <td><span style="background: #9bbb58; color: white; padding: 10px; padding-right: ${(Data.Ad_value * 10)}px">${Data.Ad_value}</span></td>
+            </tr>`;
+          }
+          else{
+            table.innerHTML = ` <tr>
+            <td>Visual (V)</td>
+            <td><span style="background: #f79647; color: white; padding: 10px; padding-right: ${(Data.V_value * 10)}px">${Data.V_value}</span></td>
+            </tr>
+            <tr>
+            <td>Auditory (A)</td>
+            <td><span style="background: #4f81bc; color: white; padding: 10px; padding-right: ${(Data.A_value * 10)}px">${Data.A_value}</span></td>
+            </tr>
+            <tr>
+            <td>Kinesthetic (K)</td>
+            <td><span style="background: #c0504e; color: white; padding: 10px; padding-right: ${(Data.K_value * 10)}px">${Data.K_value}</span></td>
+            </tr>
+            <tr>
+            <td>Auditory-digital (Ad)</td>
+            <td><span style="background: #9bbb58; color: white; padding: 10px; padding-right: ${(Data.Ad_value * 10)}px">${Data.Ad_value}</span></td>
+            </tr>`;
+          }
+          drawBasic(Data);
         })
         .catch( err => console.log('err:', err))
         // User is signed in.
@@ -45,24 +65,37 @@
 
   function drawBasic(Data) {
 
-    var data = google.visualization.arrayToDataTable([
-      ["Element", "Density"],
-      ['Auditory-digital (Ad)', Data.Ad_value],
-      ['Kinesthetic (K)',  Data.K_value],
-      ['Auditory (A)',  Data.A_value],
-      ['Visual (V)',  Data.V_value]
-    ]);
+    var data;
+    if(document.getElementById('bodyTag').classList.contains('vit')){
+      data = google.visualization.arrayToDataTable([
+        ["Element", "Tỉ trọng"],
+        ['Thính giác-kỹ thuật số (Ad)', Data.Ad_value],
+        ['Động học (K)',  Data.K_value],
+        ['Thính giác (A)',  Data.A_value],
+        ['Visual (V)',  Data.V_value]
+      ]);
+    }
+    else{
+
+      data = google.visualization.arrayToDataTable([
+        ["Element", "Density"],
+        ['Auditory-digital (Ad)', Data.Ad_value],
+        ['Kinesthetic (K)',  Data.K_value],
+        ['Auditory (A)',  Data.A_value],
+        ['Trực quan (V)',  Data.V_value]
+      ]);
+    }
 
     var options = {
-      title: 'Score of the Assesment Quiz',
+      title: 'Điểm của bài kiểm tra đánh giá',
       chartArea: {width: '50%'},
       hAxis: {
-        title: 'Total Score',
+        title: 'Tổng điểm',
         minValue: 0
       },
-      vAxis: {
-        title: 'Parameters '
-      }
+      // vAxis: {
+      //   title: 'Parameters '
+      // }
     };
 
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
